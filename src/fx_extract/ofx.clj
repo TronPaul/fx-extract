08403ba4454d82c017d83ce6476295bae35a321d
Mark McGuire <mark.b.mcg@gmail.com>
2016-06-20 07:32:46 -0500

    filter empty keys

--- src/fx_extract/ofx.clj
+++ src/fx_extract/ofx.clj
@@ -6,19 +6,27 @@
            (java.util Date)
            (java.time LocalDateTime ZoneId)))
 
-(defn read-ofx-file [ofx-file]
-  (ofx/parse ofx-file))
+(defn filter-empty [m]
+  (into {} (map (fn [[k v]]
+                  (let [v (if (map? v)
+                            (filter-empty v)
+                            v)]
+                    (if (not (or (and (coll? v) (empty? v)) (nil? v)))
+                      [k v]))) m)))
 
-(defn create-cc-details [account-number]
+(defn- create-cc-details [account-number]
   (let [cc-details (CreditCardAccountDetails.)]
     (.setAccountNumber cc-details account-number)
     cc-details))
+
+(defn read-ofx-file [ofx-file]
+  (filter-empty (ofx/parse ofx-file)))
 
 (defn read-ofx-remote [fid account-number username password]
   (let [fi (.getFinancialInstitution (FinancialInstitutionServiceImpl.) fid)
         cc-details (create-cc-details account-number)
         cc-account (.loadCreditCardAccount fi cc-details username password)]
-    (ofx/parse-data (.readStatement cc-account (Date/from (.toInstant (.atZone (LocalDateTime/of 2016 6 1 0 0) (ZoneId/systemDefault)))) (Date.)))))
+    (filter-empty (ofx/parse-data (.readStatement cc-account (Date/from (.toInstant (.atZone (LocalDateTime/of 2016 6 1 0 0) (ZoneId/systemDefault)))) (Date.))))))
 
 (defn create-fid [fi-id name organization url]
   (let [fid (BaseFinancialInstitutionData.)]
